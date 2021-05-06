b7f83bcdcbf61ec9adc44e021dcb1ea88f2c6182
antwan-twan <anthonie.meijers@hva.nl>
2021-05-06 13:12:28 +0200

    changed html and js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,25 @@
-// Add a "checked" symbol when clicking on a list item
+// Afvinken van de items
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
-}, false);
+});
 
-// Create a new list item when clicking on the "Add" button
+// Maakt een nieuw item aan wanneer je op de "toevoegen aan lijst"-knop drukt
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
+  var taak = document.createElement("li");
+  var inputValue = document.querySelector("input").value;
   var t = document.createTextNode(inputValue);
-  li.appendChild(t);
+  taak.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Je moet wel een taak toevoegen en niet op je luie reet zitten!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.querySelector("ul").appendChild(taak);
   }
-  document.getElementById("myInput").value = "";
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  document.querySelector("input").value = "";
 }
+
+// Functie toevoegen en linken aan knop
+const itemToevoegen = document.querySelector("span");
+itemToevoegen.addEventListener("click", newElement);
